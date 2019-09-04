194fc1ce4182a13bf039fb18e08e72d53b1ae63b
shier <dingzhiwei@takecloud.cn>
2019-09-04 12:43:41 +0800

    first

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          感谢a雄一路手摸手教我搭建！
+        </p>
+        <a
+          className="App-link"
+          href="https://www.mrpzx.cn/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          go a 雄 主页
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
