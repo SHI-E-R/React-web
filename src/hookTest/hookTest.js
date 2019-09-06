2ae6d797d205ea61371348fcb412f137134bd298
shier <dingzhiwei@takecloud.cn>
2019-09-06 17:20:46 +0800

    安装axios

--- src/hookTest/hookTest.js
+++ src/hookTest/hookTest.js
@@ -3,12 +3,12 @@ import React, { useState } from 'react';
 function HookTest({initialCount}) {
   const [count, setCount] = useState(1);
   return (
-    <>
+    <div>
       Count: {count}
       <button onClick={() => setCount(1)}>Reset</button>
       <button onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
       <button onClick={() => setCount(prevCount => prevCount - 1)}>-</button>
-    </>
+    </div>
   );
 }
 
